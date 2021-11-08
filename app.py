ba0fd3a8970197170bdf98fd64560519b32da271
ysyed1 <ysyed1@babson.edu>
2021-11-08 16:24:34 -0500

    working on

--- app.py
+++ app.py
@@ -2,16 +2,38 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
+
+from mbta_helper import find_stop_near
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    """This function asks for the user's location"""
+    return render_template("index.html")
+
+@app.route("/POST/nearest", methods=["POST","GET"])
+def find():
+    """
+    This function returns whether the location entered has a nearby stop and checks for wheelchair accessibility
+    """
+    if request.method == "POST":
+        place = request.form["location"]
+        place = str(place)
+        result = find_stop_near(place)
+        if result == "MBTA Not Available":
+            return render_template("notavailable.html")
+        else:
+            result = result.split(",")
+            return render_template("available.html", location = result[0], wheelchair = result[1])
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
--- mbta_helper.py
+++ mbta_helper.py
@@ -1,11 +1,17 @@
+import urllib.request
+import json
+from pprint import pprint
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
+# Add Saanya Mapquest API Key here
 MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
-
+MBTA_API_KEY = "68b2a7ae63184c6c930c23dd6940c075"
 
 # A little bit of scaffolding if you want to use it
 
@@ -14,7 +20,10 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 
 def get_lat_long(place_name):
@@ -24,7 +33,15 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    place_name = str(place_name)
+    place_name = place_name.replace(" ", "%20")
+    place_name = f"{place_name},MA"
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name}'
+    json_data = get_json(url)
+    coordinates = json_data["results"][0]["locations"][0]["latLng"]
+    latitude = coordinates["lat"]
+    longitude = coordinates["lng"]
+    return latitude, longitude
 
 
 def get_nearest_station(latitude, longitude):
@@ -34,21 +51,38 @@ def get_nearest_station(latitude, longitude):
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
     formatting requirements for the 'GET /stops' API.
     """
-    pass
+    url = f"https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}"
+    data = get_json(url)
+    try:
+        name = data["data"][0]["attributes"]["name"]
+        wheelchair_accessible = data["data"][0]["attributes"]["wheelchair_boarding"]
+    except:
+        return "MBTA Not Available"
+    if wheelchair_accessible == 0:
+        wheelchair_accessible = "No Information"
+    elif wheelchair_accessible == 1:
+        wheelchair_accessible = "Accessible"
+    else:
+        wheelchair_accessible = "Inaccessible"
+    return f"Station: {name}, Wheelchair Accessibility: {wheelchair_accessible}"
 
 
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
     """
-    pass
+    location_data = get_lat_long(place_name)
+    latitude = location_data[0]
+    longitude = location_data[1]
+    return get_nearest_station(latitude, longitude)
 
 
 def main():
     """
     You can test all the functions here
     """
-    pass
+    place = str(input("Please enter your location here: "))
+    print(find_stop_near(place))
 
 
 if __name__ == '__main__':
